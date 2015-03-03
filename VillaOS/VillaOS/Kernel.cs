0e6d6ec3e3b849cda411fa0350df8ea353290899
VillaCaleb <caleb@villa-community.com>
2015-03-03 12:42:19 -0500

    Initial Commit part 2: The Adventure Continues

---
+++ VillaOS/VillaOS/Kernel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+using System.Net.NetworkInformation;
+
+namespace VillaOS
+{
+    public class Kernel : Sys.Kernel
+    {
+
+        /*
+         TODO Alpha 1:
+         * Password and username system.
+         * Calculator
+         * Filing system
+         * and more
+         */
+        String username = "admin";
+        String password = "adminpass";
+        bool login = false;
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("VillaOS starting...");
+            Console.WriteLine("VillaOS was created by Caleb Bird and VillaOS contributors.");
+            Console.WriteLine("VillaOS started.");
+        }
+
+        protected override void Run()
+        {
+            if (login == false)
+            {
+                Console.Write("Username:");
+                var input = Console.ReadLine();
+                if (input == username)
+                {
+                    Console.WriteLine("Password:");
+                    input = Console.ReadLine();
+                    if (input == password)
+                    {
+                        Console.WriteLine("Logining in.."); // After logining in, it will load everything,
+                        login = true;
+                        Console.WriteLine("Loading Command...");
+                        Command.commands();
+
+                    }
+                }
+            }
+        }
+
+    }
+    public class Command 
+    {
+        public static void commands()
+        {
+            //This would be the main command handler.
+            var command = Console.ReadLine();
+            Console.WriteLine(command);
+            PingTimeAverage("stackoverflow.com", 4);
+        }
+        public static double PingTimeAverage(string host, int echoNum)
+        {
+            long totalTime = 0;
+            int timeout = 120;
+            Ping pingSender = new Ping();
+
+            for (int i = 0; i < echoNum; i++)
+            {
+                PingReply reply = pingSender.Send(host, timeout);
+                if (reply.Status == IPStatus.Success)
+                {
+                    totalTime += reply.RoundtripTime;
+                }
+            }
+            return totalTime / echoNum;
+        }
+    }
+}
